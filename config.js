fb6cdb879c333782b863a18a3468bedcdca51083
kg-beep88 <kgchesterlee@gmail.com>
2026-06-22 14:53:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.KG_PASS_CONFIG = {
   // Paste your Google Apps Script Web App URL here.
   // It must end with /exec
-  SCRIPT_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_EXEC_URL_HERE"
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxFARD-7YPL-iCy5ykoXmnoMIAmSqegt79-QBuFeqELY7gXmjuijXurft7xSKVpYYP3/exec"
 };
